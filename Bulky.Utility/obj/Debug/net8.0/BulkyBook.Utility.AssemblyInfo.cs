ba0fee76401b5da07a7dcce4b3d205e9b86b6fbf
HongAn <nguyenhongan.tphcm@gmail.com>
2024-08-15 10:05:43 +0700

    Azure Production Deployment

--- Bulky.Utility/obj/Debug/net8.0/BulkyBook.Utility.AssemblyInfo.cs
+++ Bulky.Utility/obj/Debug/net8.0/BulkyBook.Utility.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBook.Utility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a648e669736297700a8c391e61d4ee4465d150c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8706b2154d08015fe216d3a6a2e1a6fd3e8e01")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBook.Utility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBook.Utility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
